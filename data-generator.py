e005ac998549880e93130f0fdd621bf35e7006c8
Kenan Baralic <kenann.bara@gmail.com>
2026-06-12 16:26:41 +0200

    generate customer data

--- data-generator.py
+++ data-generator.py
@@ -1,52 +1,36 @@
 import psycopg2
-from faker import Faker
-from pathlib import Path
 import pandas as pd
-import random
+from pathlib import Path
+
 
 def main():
     BASE_DIR=Path(__file__).resolve().parent
-    #Connecting to the database
+
+    # Connecting to the database
     conn=psycopg2.connect(
         dbname='Ecommerce Analytics',
         user='postgres',
         password='samni',
         host='localhost'
     )
-    fake=Faker()
-    imported_df=pd.read_csv(BASE_DIR/'olist_products_dataset.csv')
-    products_df=pd.DataFrame()
-    products_df['product_id']=imported_df['product_id']
-    products_df['product_category']=imported_df['product_category_name']
-    product_name=pd.Series()
-    product_price=pd.Series()
-    categories=["Laptop","USB","Phone","PC","Headphones"]
-    for i in range(0,products_df.shape[0]):
-        product_name.loc[i]=fake.word().capitalize()+ " " +fake.random_element(categories)
-        product_price=random.Random()
-
-    # customer_df=pd.DataFrame()
-    # customer_df['customer_id']=imported_df['customer_unique_id']
-    # customer_df['customer_city']=imported_df['customer_city']
-    # customer_df['customer_state']=imported_df['customer_state']
-    # customer_name=pd.Series()
-    # print(customer_df)
-    # for i in range(0,customer_df.shape[0]):
-    #     customer_name.loc[i]=fake.name()
-
-    # customer_df['customer_name']=customer_name
-    # customer_df.to_csv(BASE_DIR/'customer_df.csv')
     
-    # print('INFO:',customer_df.info())
-    # print('DESCRIBE:',customer_df.describe())
-    # print('ISNULL:',customer_df[customer_df.isnull().any(axis=1)])
-    # print('DUPES:',customer_df.where(customer_df.duplicated()))
-
-
-    # cur=conn.cursor()
-    # # cur.execute('''
-    # #             SELECT * FROM schema''')
-    # # db_version=cur.fetchone()
-    # print(db_version)
+    order_item_df=pd.read_csv(BASE_DIR/'order_item_df.csv')
+    clean_df=order_item_df.drop_duplicates(subset='order_item_id')
+    
+    cur=conn.cursor()
+    for i in range(clean_df.shape[0]):
+        cur.execute("""
+            INSERT INTO schema.order_items(customer_id, customer_name, city, state)
+            VALUES (%s, %s, %s, %s)
+        """, (
+            clean_df['customer_id'].iloc[i],
+            clean_df['customer_name'].iloc[i],
+            clean_df['customer_city'].iloc[i],
+            clean_df['customer_state'].iloc[i]
+        ))
+    conn.commit()
+    
+    
+    
 if __name__=='__main__':
     main()
