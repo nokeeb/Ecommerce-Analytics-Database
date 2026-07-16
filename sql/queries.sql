c6d1afcc8aa75ce69578020c4f5c900197497b9b
Kenan Baralic <kenann.bara@gmail.com>
2026-07-16 22:56:06 +0200

    add joins

--- sql/queries.sql
+++ sql/queries.sql
@@ -34,3 +34,50 @@ GROUP BY o.order_id
 SELECT DATE_TRUNC('month',o.order_date) AS month,COUNT(o.order_id) FROM schema.orders AS o
 GROUP BY DATE_TRUNC('month',o.order_date)
 ORDER BY month
+
+
+
+
+
+--Agregate queries
+
+
+
+
+--Average consumption by customer
+SELECT c.customer_id,c.customer_name,ROUND(AVG(oi.quantity*price::numeric::integer),2) FROM schema.customers AS c
+JOIN schema.orders AS o ON o.customer_id=c.customer_id
+JOIN schema.order_items AS oi ON oi.order_id=o.order_id
+JOIN schema.products AS p ON p.product_id=oi.product_id
+GROUP BY c.customer_id
+
+--Average price by category
+SELECT p.category,ROUND(AVG(p.price::numeric::integer),2)
+FROM schema.products AS p
+GROUP BY p.category
+ORDER BY 2 DESC
+
+--Total revenue by category 
+SELECT p.category,SUM(p.price*oi.quantity) FROM schema.products AS p
+JOIN schema.order_items AS oi ON oi.product_id=p.product_id
+JOIN schema.orders AS o ON oi.order_id=o.order_id
+GROUP BY p.category 
+ORDER BY 2 DESC
+
+--Number of orders by state 
+SELECT c.state,COUNT(*) FROM schema.orders AS o
+JOIN schema.customers AS c ON o.customer_id=c.customer_id
+GROUP BY c.state
+ORDER BY 2 DESC
+
+--Top 5 orders by revenue
+SELECT o.order_id,SUM(oi.quantity*p.price::numeric::integer) FROM schema.orders AS o
+JOIN schema.order_items AS oi ON oi.order_id=o.order_id
+JOIN schema.products AS p ON p.product_id=oi.product_id
+GROUP BY o.order_id
+ORDER BY 2 DESC
+LIMIT 5;
+
+
+
+
